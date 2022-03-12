3b1d88aefc26f6e650eaea76ee528908db709e65
a.t <ameliatorresh@yahoo.com>
2022-03-12 14:50:08 -0500

    smooth ani

---
+++ typed.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+
+var typed = new Typed('.element', options);
